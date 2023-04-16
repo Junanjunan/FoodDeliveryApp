49ae9678fc730603d6993d0e5f5e3b55914ca128
taltal <taltalrealty@gmail.com>
2023-04-16 23:52:57 +0900

    DismissKeyboardView

---
+++ src/components/DismissKeyboardView.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    TouchableWithoutFeedback,
+    Keyboard,
+    StyleProp,
+    ViewStyle,
+    KeyboardAvoidingView,
+    Platform,
+} from 'react-native';
+
+const DismissKeyboardView = ({children, ...props}) => (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <KeyboardAvoidingView {...props} behavior={Platform.OS === 'android' ? 'position' : 'padding'} style={props.style}>
+      {children}
+    </KeyboardAvoidingView>
+  </TouchableWithoutFeedback>
+);
+
+export default DismissKeyboardView;
