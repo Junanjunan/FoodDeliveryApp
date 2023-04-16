8256c5e69a4cf7acaaf61632ad415b6af4f61952
taltal <taltalrealty@gmail.com>
2023-04-16 22:22:40 +0900

    Login page 02
    navigation - ScreenProps by TypeScript
    ref, useRef
    onSubmitEditing - When press enter key

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,9 +1,15 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { Alert, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../../App";
 
-function SignIn() {
+type SignInScreenProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
+
+function SignIn({ navigation }: SignInScreenProps) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const emailRef = useRef<TextInput | null>(null); // TypeScript -> generic: <TextInput | null> 이 설정 안해주면 ref={}으로 해준 부분에 TS 오류 발생
+  const passwordRef = useRef<TextInput | null>(null);
 
   const onChangeEmail = useCallback((text) => {
     setEmail(text);
@@ -15,19 +21,49 @@ function SignIn() {
   // 강의에서는 onChangeEmail, onChangePassword를 onChangeText={}안에 넣어서 했는데
   // onChangeEmail 같은거 안하고 그냥 setEmail, setPassword 해도 됨. 왜 이렇게 하는지 확인 or 질문
   const onSubmit = useCallback(() => {
-    Alert.alert('알림', '안녕~')
+    if (!email || !email.trim()) {  // trim : 좌우 공백 없애는 것 ?
+      return Alert.alert('알림', '이메일을 입력해주세요')
+    }
+    if (!password || !password.trim()) {
+      return Alert.alert('알림', '비밀번호를 입력해주세요')
+    }
+    Alert.alert('알림', '로그인 되었습니다')
   }, [])
+
+  const toSignUp = useCallback(() => {
+    navigation.navigate('SignUp');
+  }, [navigation]);
 
   const canGoNext = email && password;
   return (
     <View>
       <View>
         <Text>이메일</Text>
-        <TextInput style={styles.textInput} placeholder="이메일을 입력해주세요" onChangeText={onChangeEmail}/>
+        <TextInput style={styles.textInput} placeholder="이메일을 입력해주세요" onChangeText={onChangeEmail}
+          value={email}
+          importantForAutofill="yes"
+          autoComplete="email"
+          textContentType="emailAddress"
+          keyboardType="email-address"
+          returnKeyType="next"
+          ref={emailRef}
+          onSubmitEditing={() => {
+            passwordRef.current?.focus();
+          }}
+          blurOnSubmit={false}
+        />
       </View>
       <View>
         <Text>비밀번호</Text>
-        <TextInput style={styles.textInput} placeholder="비밀번호를 입력해주세요" onChangeText={onChangePassword}/>
+        <TextInput style={styles.textInput} placeholder="비밀번호를 입력해주세요" onChangeText={onChangePassword}
+          value={password}
+          secureTextEntry
+          importantForAutofill="yes"
+          autoComplete="password"
+          textContentType="password"
+          ref={passwordRef}
+          onSubmitEditing={onSubmit}
+        />
     </View>
       <View style={styles.buttonZone}>
         <Pressable onPress={onSubmit} style={!canGoNext ? styles.loginButton : StyleSheet.compose(styles.loginButton, styles.loginButtonActive)} disabled={!canGoNext}>
